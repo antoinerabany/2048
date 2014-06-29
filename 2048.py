b9f990676ce79b07c16995b9fd09a809156d9b6b
Elancar <sebastien.bereiziat@hotmail.fr>
2014-06-29 16:09:20 +0200

    Quelques commentaires + lisibilité + idées pour le random en commntaire.

--- 2048.py
+++ 2048.py
@@ -6,9 +6,11 @@ from pygame.locals import *
 import random
 
 def main():
+
     pygame.init()
     window = pygame.display.set_mode((400, 400))
     font = pygame.font.Font(None,90)
+
     jeu = 1
 
     fond = pygame.image.load("background.jpg").convert()
@@ -20,40 +22,53 @@ def main():
 
 
     while jeu:
+
         for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
-            if event.type == QUIT:     #Si un de ces événements est de type QUIT
-                jeu = 0      #On arrête la boucle
+
+            if event.type == QUIT:   
+
+                jeu = 0
 
             if event.type == KEYDOWN:
+
                 if event.key == K_DOWN:
+
                     board = move(board,'down')
+
                 if event.key == K_UP:
+
                     board = move(board,'up')
+
                 if event.key == K_RIGHT:
+
                     board = move(board,'right')
+
                 if event.key == K_LEFT:
+
                     board = move(board,'left')
-                if defeat(board) == 1:
+
+                if defeat(board) == 1: #Pas ultra OK avec ça.
+
                     jeu = 0
+
                 else:
-                    board = addTile(board)
+
+                    board = addTile(board) #Celui ci non plus.
 
 
         window.blit(fond, (0,0))
+
         for i,line in enumerate(board):
+
             for j,val in enumerate(line):
+
                 if val != None:
+
                     window.blit(font.render(str(val), 1, (255,0,0)), (j*100,i*100))
 
         pygame.display.flip()
 
-
-
-
-
-
-
-def move(board,direction):
+def move(board,direction): #Fonction principale pour faire bouger les pieces.
 
     if direction == 'left':
         
@@ -80,43 +95,43 @@ def move(board,direction):
         board = transpose(move(transpose(board),'right'))
 
     else:
+
         print('error')
 
     return board
 
+def fusion(line): #Fonction qui permet de fusionner les pieces.
 
+    fusion = 0 #Permet de ne pas tout fusionner d'un coup.
 
-
-
-def fusion(line):
-
-    fusion = 0
-
-    #A modifier je pense
     for j,tile in enumerate(line):
 
         for n in range(j):
 
             if tile == None:
+
                 pass
 
             else: 
 
                 if  line[j-n-1] == None:
+
                     line[j-n-1] = line[j-n]
                     line[j-n] = None
 
                 if line[j-n-1] != None:
 
                     if line[j-n-1] == line[j-n] and fusion == 0:
+
                         line[j-n-1] = 2*line[j-n]
                         line[j-n] = None
                         fusion = 1
 
                     else: # board[i][j-1] != tile
+
                         pass
 
-def transpose(board):
+def transpose(board): #Transpose la matrice "board" pour simplifier les mouvements.
 
     lineR = []
     rotated = []
@@ -132,20 +147,21 @@ def transpose(board):
 
     return rotated
 
-def addTile(board):
+def addTile(board): #Ajoute une pieces de façon random.
 
     i = int(random.random()*4)
     j = int(random.random()*4)
 
     while board[i][j] != None:
+
          i = int(random.random()*4)
          j = int(random.random()*4)
 
-    board[i][j] = 2
+    board[i][j] = 2 # <= foutre un random 80-20 entre 2 et 4 ici
 
     return board
 
-def defeat(board):
+def defeat(board): #Fonction qui parametre la defaite.
 
     compteur = 0
 
@@ -155,21 +171,15 @@ def defeat(board):
 
             if board[i][j] != None:
 
-                compteur += compteur
+                compteur += 1
+                #compteur += compteur #Plutôt "compteur += 1" non ? Tu double sa valeur en partant de 0.
 
     if compteur == (len(board[0]) * len(board)):
+
         return 1
+
     else:
         return 0
 
-
-
-
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
